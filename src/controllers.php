ee1a3033702bbfcfe8c68d6a25f63ba46d646383
MRxRafi <rafadriveclase@gmail.com>
2020-12-05 18:17:05 +0100

    Added some routes, prepared for HTML

--- src/controllers.php
+++ src/controllers.php
@@ -8,6 +8,7 @@
  * @link     http://www.etsisi.upm.es/ ETS de Ingeniería de Sistemas Informáticos
  */
 
+use MiW\Results\Entity\Result;
 use MiW\Results\Entity\User;
 use MiW\Results\Utility\Utils;
 
@@ -15,10 +16,27 @@ function funcionHomePage()
 {
     global $routes;
 
-    $rutaListado = $routes->get('ruta_user_list')->getPath();
+    $rutaListadoUsers = $routes->get('ruta_user_list')->getPath();
+    $rutaCreateUser = $routes->get('ruta_create_user')->getPath();
+    $rutaUpdateUser = $routes->get('ruta_update_user')->getPath();
+    $rutaListadoResults = $routes->get('ruta_result_list')->getPath();
+    $rutaCreateResult = $routes->get('ruta_create_result')->getPath();
+    $rutaUpdateResult = $routes->get('ruta_update_result')->getPath();
     echo <<< ____MARCA_FIN
+    <h1>RUTAS</h1>
     <ul>
-        <li><a href="$rutaListado">Listado Usuarios</a></li>
+        <li><a href="$rutaListadoUsers">Listado Usuarios</a></li>
+        <br>
+        <li><a href="$rutaCreateUser">Crear Usuario</a></li>
+        <br>
+        <li><a href="$rutaUpdateUser">Actualizar Usuario</a></li>
+        <br>
+        <li><a href="$rutaListadoResults">Listado Resultados</a></li>
+        <br>
+        <li><a href="$rutaCreateResult">Crear Resultado</a></li>
+        <br>
+        <li><a href="$rutaUpdateResult">Actualizar Resultado</a></li>
+        <br>
     </ul>
 ____MARCA_FIN;
 }
@@ -29,10 +47,53 @@ function funcionListadoUsuarios(): void
 
     $userRepository = $entityManager->getRepository(User::class);
     $users = $userRepository->findAll();
-    var_dump($users);
+
+    echo <<< __MARCA_FIN
+        <h1>USUARIOS</h1>
+            <ul>
+__MARCA_FIN;
+    foreach ($users as $user) {
+        echo "<li>$user</li>" . PHP_EOL;
+    }
+    echo '</ul>';
+}
+
+function funcionCrearUsuario() {
+    echo 'Crear usuario funciona';
+}
+
+function funcionActualizarUsuario(string $name) {
+    echo 'Actualizar usuario funciona ' . $name;
 }
 
 function funcionUsuario(string $name)
 {
     echo $name;
 }
+
+function funcionListadoResultados(): void
+{
+    $entityManager = Utils::getEntityManager();
+
+    $resultRepository = $entityManager->getRepository(Result::class);
+    $results = $resultRepository->findAll();
+
+    echo <<< __MARCA_FIN
+        <h1>RESULTADOS</h1>
+            <ul>
+__MARCA_FIN;
+    foreach ($results as $result) {
+        echo "<li>$result</li>" . PHP_EOL;
+    }
+    echo '</ul>';
+}
+
+function funcionCrearResultado() {
+    echo 'Crear resultado funciona';
+}
+
+function funcionActualizarResultado(string $name) {
+    echo 'Actualizar resultado funciona ' . $name;
+}
+
+
