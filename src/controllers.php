36633e89873820949fa0c7204092dc0062e1746e
MRxRafi <rafadriveclase@gmail.com>
2020-12-07 17:54:28 +0100

    Operaciones de actualizado por HTML incorporadas. Práctica finalizada

--- src/controllers.php
+++ src/controllers.php
@@ -65,7 +65,7 @@ __MARCA_FIN;
                     <a href="/users/$username">Borrar Usuario</a>
                 </td>
                 <td>
-                    <a href="/users/update/$username">Actualizar Resultado</a>
+                    <a href="/users/update/$username">Actualizar Usuario</a>
                 </td>
             </tr>
 ____MARCA_FIN;
@@ -81,13 +81,13 @@ function funcionCrearUsuario() {
         <form method="post">
             <fieldset>
                 <legend>Alta usuario</legend>
-                <label for="username">Inserte un nombre de usuario</label>
+                <label for="username">Inserte un nombre de usuario: </label>
                 <input type="text" name="username" placeholder="Nombre de usuario">
                 <br>
-                <label for="email">Inserte un email</label>
+                <label for="email">Inserte un email: </label>
                 <input type="email" name="email" placeholder="Email">
                 <br>
-                <label for="password">Inserte una contraseña</label>
+                <label for="password">Inserte una contraseña: </label>
                 <input type="password" name="password" placeholder="Contraseña">
                 <br>
                 <button type="submit">Enviar</button>
@@ -118,7 +118,56 @@ ___MARCA_FIN;
 }
 
 function funcionActualizarUsuario(string $name) {
-    echo 'Actualizar usuario funciona ' . $name;
+    $entityManager = Utils::getEntityManager();
+    $userRepository = $entityManager->getRepository(User::class);
+    $oldUser = $userRepository->findOneBy(['username' => $name]);
+    if ($oldUser != null) {
+        if (empty($_POST)){
+            echo <<< ___MARCA_FIN
+            <form method="post">
+                <fieldset>
+                    <legend>Actualizar usuario $name</legend>
+                    <label for="username">Inserte un nuevo nombre de usuario: </label>
+                    <input type="text" name="username" placeholder="Nombre de usuario">
+                    <br>
+                    <label for="email">Inserte un nuevo email: </label>
+                    <input type="email" name="email" placeholder="Email">
+                    <br>
+                    <label for="password">Inserte una nueva contraseña: </label>
+                    <input type="password" name="password" placeholder="Contraseña">
+                    <br>
+                    <button type="submit">Enviar</button>
+                </fieldset>
+            </form>
+___MARCA_FIN;
+        } else {
+            if ($_POST['username'] != null) {
+                $oldUser->setUsername($_POST['username']);
+            }
+            if ($_POST['email'] != null) {
+                $oldUser->setEmail($_POST['email']);
+            }
+            if ($_POST['password'] != null) {
+                $oldUser->setPassword($_POST['password']);
+            }
+
+            if ($userRepository->findOneBy(['username' => $_POST['username']]) !== null ||
+                $userRepository->findOneBy(['email' => $_POST['email']]) !== null) {
+                echo '<p>El usuario o email ya existe en la BBDD</p>';
+            } else {
+                try {
+                    $entityManager->merge($oldUser);
+                    $entityManager->flush();
+
+                    echo '<p>Usuario actualizado correctamente</p>';
+                } catch(Throwable $t) {
+                    echo $t->getMessage();
+                }
+            }
+        }
+    }
+
+    echo '<br><a href="/">Volver a home</a>';
 }
 
 function funcionUsuario(string $name)
@@ -158,6 +207,7 @@ function funcionListadoResultados(): void
                     <th>Result</th>
                     <th>UserId</th>
                     <th>Username</th>
+                    <th>Date</th>
                 </thead>
                 <tbody>
 __MARCA_FIN;
@@ -167,12 +217,14 @@ __MARCA_FIN;
         $user = $result->getUser();
         $userId = $user->getId();
         $username = $user->getUsername();
+        $date = $result->getTime()->format('Y-m-d H:i:s');
         echo <<< ____MARCA_FIN
             <tr>
                 <td>$id</td>
                 <td>$resultId</td>
                 <td>$userId</td>
                 <td>$username</td>
+                <td>$date</td>
                 <td>
                     <a href="/results/$resultId">Borrar Resultado</a>
                 </td>
@@ -200,7 +252,7 @@ function funcionCrearResultado() {
                 <label for="userId">Inserte el ID del usuario asociado: </label>
                 <input type="number" name="userId" placeholder="ID usuario">
                 <br>
-                <label for="fecha">Inserte una fecha (opcional): </label>
+                <label for="fecha">Inserte una fecha: </label>
                 <input type="date" name="fecha" placeholder="Fecha">
                 <br>
                 <button type="submit">Enviar</button>
@@ -240,7 +292,66 @@ ___MARCA_FIN;
 }
 
 function funcionActualizarResultado(string $name) {
-    echo 'Actualizar resultado funciona ' . $name;
+    $entityManager = Utils::getEntityManager();
+    $resultRepository = $entityManager->getRepository(Result::class);
+    $oldResult = $resultRepository->findOneBy(['result' => $name]);
+    if ($oldResult != null) {
+        if (empty($_POST)){
+            echo <<< ___MARCA_FIN
+            <form method="post">
+                <fieldset>
+                    <legend>Actualizar resultado $name</legend>
+                    <label for="resultId">Inserte un nuevo id para el resultado: </label>
+                    <input type="number" name="resultId" placeholder="ID resultado">
+                    <br>
+                    <label for="userId">Inserte el nuevo ID del usuario asociado: </label>
+                    <input type="number" name="userId" placeholder="ID usuario">
+                    <br>
+                    <label for="fecha">Inserte una nueva fecha: </label>
+                    <input type="date" name="fecha" placeholder="Fecha">
+                    <br>
+                    <button type="submit">Enviar</button>
+                </fieldset>
+            </form>
+___MARCA_FIN;
+        } else {
+            $userExists = true;
+            try {
+                if ($_POST['resultId'] != null) {
+                    $oldResult->setResult($_POST['resultId']);
+                }
+                if ($_POST['userId'] != null) {
+                    $user = $entityManager->getRepository(User::class)->find($_POST['userId']);
+                    if($user !== null) {
+                        $oldResult->setUser($user);
+                    } else {
+                        $userExists = false;
+                    }
+                }
+                if ($_POST['fecha'] !== null) {
+                    $oldResult->setTime(new DateTime($_POST['fecha']));
+                }
+            } catch (Throwable $t) {
+                echo $t->getMessage();
+            }
+            if ($resultRepository->findOneBy(['result' => $_POST['resultId']]) !== null) {
+                echo '<p>El resultado ya existe en la BBDD</p>';
+            } else if(!$userExists) {
+                echo '<p>El usuario introducido no existe</p>';
+            } else {
+                try {
+                    $entityManager->merge($oldResult);
+                    $entityManager->flush();
+
+                    echo '<p>Resultado actualizado correctamente</p>';
+                } catch(Throwable $t) {
+                    echo $t->getMessage();
+                }
+            }
+        }
+    }
+
+    echo '<br><a href="/">Volver a home</a>';
 }
 
 function funcionResultado(string $name) {
